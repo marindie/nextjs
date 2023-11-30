3259f342c200845beea454a801103cfbab9e0a66
wony <marindie@naver.com>
2023-11-30 20:25:23 +0900

    update

---
+++ src/app/flex/page.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+const FlexItem = () => {
+  return (
+    <div>
+      <div className='flex_container'>
+        <div className='item item1'>1</div>
+        <div className='item item2'>2</div>
+        <div className='item item3'>3</div>
+        <div className='item item4'>4</div>
+        <div className='item item5'>5</div>
+        <div className='item item6'>6</div>
+        <div className='item item7'>7</div>
+        <div className='item item8'>8</div>
+        <div className='item item9'>9</div>
+        <div className='item item10'>10</div>
+        <div className='item item1'>1</div>
+        <div className='item item2'>2</div>
+        <div className='item item3'>3</div>
+        <div className='item item4'>4</div>
+        <div className='item item5'>5</div>
+        <div className='item item6'>6</div>
+        <div className='item item7'>7</div>
+        <div className='item item8'>8</div>
+        <div className='item item9'>9</div>
+        <div className='item item10'>10</div>
+        <div className='item item1'>1</div>
+        <div className='item item2'>2</div>
+        <div className='item item3'>3</div>
+        <div className='item item4'>4</div>
+        <div className='item item5'>5</div>
+        <div className='item item6'>6</div>
+        <div className='item item7'>7</div>
+        <div className='item item8'>8</div>
+        <div className='item item9'>9</div>
+        <div className='item item10'>10</div>
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default FlexItem
