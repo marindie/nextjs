2ebcc7b959652f455891a375beb8e1c0132fbd63
wony <marindie@naver.com>
2023-12-06 19:58:37 +0900

    update

---
+++ src/app/common/comp/menu/KylieCosmeticMenu.tsx
@@ -0,0 +1,19 @@
+const KylieCosmeticMenu = () => {
+  return (
+    <div className='menu_wrap_01'>
+      <nav className='navigation'>
+        <ul className='flex'>
+          <li>Cosmetic</li>
+          <li>Skin</li>
+          <li>Baby</li>
+          <li>About</li>
+          <li>Reward</li>
+        </ul>
+      </nav>
+      <div className='logo_box'></div>
+      <div className='icon_box'></div>
+    </div>
+  )
+}
+
+export default KylieCosmeticMenu
